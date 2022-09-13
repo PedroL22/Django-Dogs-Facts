6fdb551c9134eda36703f51c31ca61facca6fc56
Pedro L <pedrocontact22@gmail.com>
2022-09-13 15:58:52 -0300

    Added Dog API from ElliottLandsborough and Dog API from kinduff

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def home(request):
-	return render(request, 'index.html')
+	dog_image = requests.get('https://dog.ceo/api/breeds/image/random').json()
+	dog_fact = requests.get('http://dog-api.kinduff.com/api/facts').json()
+	return render(request, 'index.html', {'dog_fact':dog_fact, 'dog_image':dog_image})
